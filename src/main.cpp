0bbae460e77aebf7fd539513a7d3b9e65cd9e2e5
Pawel Trepka <trepka.pawel@gmail.com>
2024-01-28 19:36:56 +0100

    port selector platformio.ini and debug flag

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
   #include <avr/power.h>
 #endif
 
+// Debug, see in platformio.ini
+
 // Define terminal baud rate
 #define TERMBAUD 115200
 
@@ -27,6 +29,64 @@ void rgbOff() {
   pixels.clear();
   pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(0, 0, 0));
   pixels.show();
+  pixels.clear();
+}
+
+void show_rgb () {
+  for(int i=0; i<NUM_COLORS; i++) {
+    switch (i) {
+      case (0):
+      // RED
+        delay(DELAYVAL);
+#ifdef DEBUG
+        Serial.printf("RED rgb LED, %d\n", i);
+#endif
+        pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(150, 0, 0));
+        break;
+      case (1):
+      // GREEN
+        delay(DELAYVAL);
+#ifdef DEBUG
+        Serial.printf("GREEN rgb LED %d\n", i);
+#endif
+        pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(0, 150, 0));
+        break;
+      case (2):
+      // BLUE
+        delay(DELAYVAL);
+#ifdef DEBUG
+        Serial.printf("BLUE rgb LED %d\n", i);
+#endif
+        pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(0, 0, 150));
+        break;
+      default:
+      // WHITE
+        delay(DELAYVAL);
+#ifdef DEBUG
+        Serial.printf("WHITE rgb LED %d\n", i);
+#endif
+        pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(15, 15, 15));
+        break;
+    }
+    pixels.show();
+    delay(DELAYVAL);
+    pixels.clear();
+  }
+}
+
+void rainbow() {
+  int mydelay = 25;
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<pixels.numPixels(); i++) { 
+      int pixelHue = firstPixelHue + ((i * 65536L) / pixels.numPixels());
+      pixels.setPixelColor(BUILTIN_RGB_LED, pixels.gamma32(pixels.ColorHSV(pixelHue)));
+#ifdef DEBUG
+      Serial.printf("PixelHue %d, GAMMA: %d, firstPixelHue: %d, numpixels %d\n", pixelHue, pixels.gamma32(pixels.ColorHSV(pixelHue)), firstPixelHue, pixels.numPixels());
+#endif
+      pixels.show();
+    }
+    delay(mydelay);
+  }
 }
 
 void setup() {
@@ -42,40 +102,23 @@ void setup() {
 }
 
 void loop() {
-  for(int i=0; i<NUM_COLORS; i++) {
-    switch (i) {
-      case (0):
-      // RED
-        pixels.clear();
-        delay(DELAYVAL);
-        Serial.printf("RED rgb LED, %d\n", i);
-        pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(150, 0, 0));
-        break;
-      case (1):
-      // GREEN
-        pixels.clear();
-        delay(DELAYVAL);
-        Serial.printf("GREEN rgb LED %d\n", i);
-        pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(0, 150, 0));
-        break;
-      case (2):
-      // BLUE
-        pixels.clear();
-        delay(DELAYVAL);
-        Serial.printf("BLUE rgb LED %d\n", i);
-        pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(0, 0, 150));
-        break;
-      default:
-        pixels.clear();
-        delay(DELAYVAL);
-        Serial.printf("WHITE rgb LED %d\n", i);
-        pixels.setPixelColor(BUILTIN_RGB_LED, pixels.Color(15, 15, 15));
-        while(1)
-        break;
-    }
-    pixels.show();
-    delay(DELAYVAL);
-  }
+#ifdef DEBUG
+  Serial.printf("Start RGB switch\n");
+#endif
+  show_rgb();
+#ifdef DEBUG
+  Serial.printf("RGB mode OFF\n");
+#endif
+  rgbOff();
+  delay(DELAYVAL);
+  delay(DELAYVAL);
+#ifdef DEBUG
+  Serial.printf("Start RGB Rainbow %d\n", pixels.numPixels());
+#endif
+  rainbow();
+#ifdef DEBUG
+  Serial.printf("RGB mode OFF\n");
+#endif
   rgbOff();
   while(1);
  }
